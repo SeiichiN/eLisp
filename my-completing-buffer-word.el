2e0c117847fbbce1f956d19f78c5b11438622096
SeiichiN <billie175@gmail.com>
2020-02-10 23:18:23 +0900

    dekitakana

--- my-completing-buffer-word.el
+++ my-completing-buffer-word.el
@@ -8,14 +8,13 @@
     (goto-char (point-min))
     (let (word word-alist)
       (while (not (eobp))
-        (re-search-forward "^[a-zA-z]+" nil t)
+        (re-search-forward "^[A-Za-z]+" nil t)
         (setq word (match-string 0))
-        (append word word-alist)
+        (setq word-alist (cons (list word) word-alist))
         (forward-line 1))
-      (message word-alist)
-      ;(completing-read "単語?:" word-alist)
-      )
-    ))
+      ;(message word-alist)
+      (completing-read "単語?:" word-alist nil 1)
+      )))
 
 
 
